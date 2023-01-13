3a423f9ff320750126541f6dcb29f4f434602d0e
Sarbjeet <sarbjeetkaur310@gmail.com>
2023-01-12 23:07:28 -0500

    demo

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,13 +1,20 @@
 def call() {
     node {
 
+        stage ('checkout code') {
+            git branch: 'main', url: 'https://github.com/Skaur501/cart.git'
+        }
+
         stage ('code quality') {
+            echo 'code quality'
         }
 
         stage ('Test cases') {
+            echo 'Test cases'
         }
 
         stage ('Publish a release') {
+            echo 'Publish a release'
         }
     }
 }

9a5911768411e6275e5f289e59e7e008b1ed744b
Sarbjeet <sarbjeetkaur310@gmail.com>
2023-01-15 00:19:49 -0500

    demo

--- vars/java.groovy
+++ vars/java.groovy
@@ -7,7 +7,6 @@ def call() {
             common.release()
         }
         catch (errors) {
-            mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: 'sarbjeetkaur310@gmail.com', mimeType: 'text/html', replyTo: 'sarbjeetkaur310@gmail.com', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "sarbjeetkaur310@gmail.com";
         }
     }
 }
